151520bb0096e662f37d51c363cafda7ee3ae11b
MadeleineSmith <madeleinesmith1995@gmail.com>
2017-02-09 15:02:48 +0000

    eslinted

--- myscripts.js
+++ myscripts.js
@@ -1,112 +1,90 @@
-function callback(data) {
-
-    if (sessionStorage.array === undefined) {
-        sessionStorage.array = [];
-
-    }
-    setUpImages(data);
-
-}
-
 /**
  * Returns a div with the class specified
  */
-function createBootstrapDiv(x) {
-    var div = document.createElement('DIV');
-    div.setAttribute("class", x);
+function createBootstrapDiv (x) {
+  var div = document.createElement('DIV')
+  div.setAttribute('class', x)
 
-    return div;
+  return div
 }
 
-
-function setUpImages(data) {
-    var htmlDiv = document.querySelector('.container-fluid');
-    var rowDiv = createBootstrapDiv("row");
-
-
-    for (var i = 0; i < data["items"].length; i++) {
-
-        var img = document.createElement('IMG');
-        img.src = data["items"][i]["media"]["m"];
-        img.alt = data["items"][i]["title"];
-
-
-
-
-        var columnDiv = createBootstrapDiv("col-sm-3");
-        columnDiv.appendChild(img);
-
-        columnDiv.addEventListener('click', toggleSelected, false);
-
-        if (sessionStorage.array === "") {
-            var sesh = [];
-        }
-        else {
-            var sesh = sessionStorage.array.split(",");
-
-        }
-
-        if (sesh.indexOf(img.getAttribute("src")) !== -1) {
-            img.parentNode.classList.add("selected");
-        }
-
-        rowDiv.appendChild(columnDiv);
-
-    }
-    htmlDiv.appendChild(rowDiv);
-
-
+function callback(data) {
+  if (window.sessionStorage.array === undefined) {
+    window.sessionStorage.array = []
+  }
+  setUpImages(data)
 }
 
-function toggleSelected(e) {
-        if (e.target.parentNode.classList.length === 1 || e.target.parentNode.classList.contains("deselected")) {
+function setUpImages (data) {
+  var htmlDiv = document.querySelector('.container-fluid')
+  var rowDiv = createBootstrapDiv('row')
 
+  for (var i = 0; i < data['items'].length; i++) {
+    var img = document.createElement('IMG')
+    img.src = data['items'][i]['media']['m']
+    img.alt = data['items'][i]['title']
 
-            if (!e.target.parentNode.classList.contains("row")) {
+    var columnDiv = createBootstrapDiv('col-sm-3')
+    columnDiv.appendChild(img)
 
-                if (e.target.parentNode.classList.contains("deselected")) {
-                    e.target.parentNode.classList.remove("deselected");
-                }
-
-                e.target.parentNode.classList.add("selected");
-
-                if (sessionStorage.array === "") {
-                    sesh = [];
-                }
-                else {
-                    sesh = sessionStorage.array.split(",");
-                }
-
-                sesh.push(e.target.getAttribute("src"));
-
-                sessionStorage.array = sesh;
-            }
-
-
-
-        }
-        else {
-            e.target.parentNode.classList.remove("selected");
-
-            e.target.parentNode.classList.add("deselected");
-
-            var sesh = sessionStorage.array.split(",");
-
-            var index = sesh.indexOf(e.target.getAttribute("src"));
-
-            if (index !== -1) {
-                sesh.splice(index, 1);
-            }
-
-            sessionStorage.array = sesh;
-            console.log(sessionStorage.array);
-
-        }
+    var sesh
+    if (window.sessionStorage.array === '') {
+      sesh = []
+    } else {
+      sesh = window.sessionStorage.array.split(',')
     }
+
+    columnDiv.addEventListener('click', toggleSelected, false)
+    if (sesh.indexOf(img.getAttribute('src')) !== -1) {
+      img.parentNode.classList.add('selected')
+    }
+
+    rowDiv.appendChild(columnDiv)
+  }
+  htmlDiv.appendChild(rowDiv)
+}
+
+function toggleSelected (e) {
+  if (e.target.parentNode.classList.length === 1 || e.target.parentNode.classList.contains('deselected')) {
+    var sesh
+
+    if (!e.target.parentNode.classList.contains('row')) {
+      if (e.target.parentNode.classList.contains('deselected')) {
+        e.target.parentNode.classList.remove('deselected')
+      }
+
+      e.target.parentNode.classList.add('selected')
+      if (window.sessionStorage.array === '') {
+        sesh = []
+      } else {
+        sesh = window.sessionStorage.array.split(',')
+      }
+
+      sesh.push(e.target.getAttribute('src'))
+
+      window.sessionStorage.array = sesh
+    }
+  } else {
+    e.target.parentNode.classList.remove('selected')
+
+    e.target.parentNode.classList.add('deselected')
+
+    sesh = window.sessionStorage.array.split(',')
+
+    var index = sesh.indexOf(e.target.getAttribute('src'))
+
+    if (index !== -1) {
+      sesh.splice(index, 1)
+    }
+
+    window.sessionStorage.array = sesh
+    console.log(window.sessionStorage.array)
+  }
+}
 
 (function () {
-    var tags = 'london';
-    var script = document.createElement('script');
-    script.src = 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=callback& tags=' + tags;
-    document.head.appendChild(script);
-})();
+  var tags = 'london'
+  var script = document.createElement('script')
+  script.src = 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=callback& tags=' + tags
+  document.head.appendChild(script)
+})()
